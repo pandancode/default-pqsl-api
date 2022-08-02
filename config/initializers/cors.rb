333274454244a8f3b2275d17a797093a4ef42d3a
yilun chen <yilun.chen@web.de>
2022-08-02 12:47:04 +0100

    Updated cors file with no success

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins '*'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # headers: :any,
+      headers: %w(Authorization),
+      expose: %w(Authorization),
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      max_age: 600
   end
 end
