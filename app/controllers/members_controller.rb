b5a38cee7d57e64f221f19e00d033281fad2386a
yilun chen <yilun.chen@web.de>
2022-08-16 15:58:35 +0100

    Now working

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,7 @@
 class MembersController < ApplicationController
-  after_action :authenticate_user!
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def show
-    p "Current user coming"
-    p current_user
-    p "Current user done"
-    p ENV['DEVISE_JWT_SECRET_KEY']
     render json: { message: "If you see this, you're in!" }
   end
 end
